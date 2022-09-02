0f03ea477602eabbd700cf91d8c152598cfd3b36
elif rumeysa aydin <elifrumeysaaydin@gmail.com>
2022-09-02 04:49:30 +0300

    Add javascript

---
+++ main.js
@@ -0,0 +1,20 @@
+let small_shoes = document.querySelectorAll(".small_shoes img");
+let bigger_image = document.querySelector(".bigger_image img");
+let shadow= document.querySelector(".shadow");
+let shoes_word=document.querySelector(".shoes_word")
+
+
+small_shoes.forEach((el)=>{
+el.addEventListener("click", (e)=>{
+    small_shoes.forEach((li2)=>{
+        li2.classList.remove("active")
+    })
+
+    e.target.classList.add("active");
+    e.target.style.borderColor=e.target.dataset.bg;
+
+    bigger_image.src=e.target.src;
+    shadow.style.backgraund=e.target.dataset.bg;
+    shoes_word.style.color=e.target.dataset.bg;
+})
+})
